225f5e0a101fc548a7dbe6093a000d050c3bac69
Vihaan <139734746+vrs31quidditchseeker@users.noreply.github.com>
2023-09-22 12:25:13 -0700

    Add files via upload

---
+++ Run Codes/VihaanAarush.py
@@ -0,0 +1,29 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase, GyroDriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+left_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.D)
+
+drive_base = GyroDriveBase(left_motor, right_motor, wheel_diameter=62.5, axle_track=136)
+drive_base.settings(300,500,100,200)
+
+Right_arm = Motor(Port.E, Direction.COUNTERCLOCKWISE, [24, 12, 12, 24])
+Left_arm = Motor(Port.A, Direction.COUNTERCLOCKWISE, [24, 12, 12, 24])
+
+speed = 100.5
+
+#Code Starts here!
+drive_base.straight(250)
+#drive_base.turn(80)
+#drive_base.straight(390)
+#drive_base.straight(-90)
+#drive_base.turn(15)
+#drive_base.straight(375)
+#drive_base.turn(-90)
+#drive_base.straight(350)
+#drive_base.turn(270)
