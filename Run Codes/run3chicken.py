6977ced7a1b2d56a1263dd731d4864fe4e6a34ae
m-penguin <140285343+m-penguin@users.noreply.github.com>
2023-11-10 13:56:22 -0800

    Create run3chicken.py

---
+++ Run Codes/run3chicken.py
@@ -0,0 +1,35 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase, GyroDriveBase
+from pybricks.tools import wait, StopWatch
+from pybricks.tools import hub_menu
+
+hub = PrimeHub()
+
+left_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.D)
+
+drive_base = GyroDriveBase(left_motor, right_motor, wheel_diameter=62.5, axle_track=136)
+drive_base.settings(300,600,100,200)
+
+
+Right_arm = Motor(Port.E, Direction.COUNTERCLOCKWISE, [24, 12, 12, 24])
+Left_arm = Motor(Port.A, Direction.COUNTERCLOCKWISE, [24, 12, 12, 24])
+
+speed = 500
+
+#Code Starts here!
+drive_base.curve(100, -35)
+drive_base.straight(240)
+Right_arm.run_target(300, -400)
+drive_base.settings(150,600,100,200)
+drive_base.turn(-0.5)
+drive_base.straight(124)
+Left_arm.run_target(1000, 3000)
+drive_base.curve(-280,12)
+drive_base.straight(-100)
+drive_base.curve(280,12)
+
+drive_base.settings(700,900,100,200)
+drive_base.straight(-400)
