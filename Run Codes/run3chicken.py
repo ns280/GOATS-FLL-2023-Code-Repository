443ef7703c1dfd0f464eb25ec372374c505b4f97
NOTAARUSH12 <141258762+NOTAARUSH12@users.noreply.github.com>
2023-11-10 17:56:47 -0800

    Update run3chicken.py
    1+1=3

--- Run Codes/run3chicken.py
+++ Run Codes/run3chicken.py
@@ -17,19 +17,22 @@ drive_base.settings(300,600,100,200)
 Right_arm = Motor(Port.E, Direction.COUNTERCLOCKWISE, [24, 12, 12, 24])
 Left_arm = Motor(Port.A, Direction.COUNTERCLOCKWISE, [24, 12, 12, 24])
 
-speed = 500
+speed = 700
 
 #Code Starts here!
 drive_base.curve(100, -35)
 drive_base.straight(240)
-Right_arm.run_target(300, -400)
+Right_arm.run_target(300, -400,wait=False)
+wait(300)
 drive_base.settings(150,600,100,200)
 drive_base.turn(-0.5)
-drive_base.straight(124)
+drive_base.straight(122)
 Left_arm.run_target(1000, 3000)
-drive_base.curve(-280,12)
+drive_base.turn(-8)
 drive_base.straight(-100)
-drive_base.curve(280,12)
-
-drive_base.settings(700,900,100,200)
-drive_base.straight(-400)
+drive_base.curve(320,12)
+drive_base.settings(300,600,100,200)
+drive_base.settings(999, 999, 999, 999)
+drive_base.straight(-150)
+drive_base.turn(-220)
+drive_base.straight(300)
