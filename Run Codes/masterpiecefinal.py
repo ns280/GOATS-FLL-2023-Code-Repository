0d408ced3a04929605cd95727a0dadb48b795166
Vihaan <139734746+vrs31@users.noreply.github.com>
2023-11-12 17:11:32 -0800

    Update masterpiecefinal.py

--- Run Codes/masterpiecefinal.py
+++ Run Codes/masterpiecefinal.py
@@ -182,21 +182,37 @@ def Aarush_Vihaan():
 def A_M():
   #Run 2 Code
   #Code Starts here!
+    #Code Starts here!
     drive_base.curve(100, -35)
-    drive_base.straight(240)
-    Right_arm.run_target(300, -400,wait=False)
-    wait(300)
+#Reach the girl
+drive_base.straight(240)
+#Pickup girl
+    Right_arm.run_target(500, -400)
+#updating the speed
     drive_base.settings(150,600,100,200)
-    drive_base.turn(-0.5)
-    drive_base.straight(122)
+#commenting the turn to test it out
+#drive_base.turn(-1)
+#Reach Chicken gear
+    drive_base.straight(123)
+#Moving the chicken
     Left_arm.run_target(1000, 3000)
+#Aligning to lift the latch
     drive_base.turn(-8)
-    drive_base.straight(-100)
-    drive_base.curve(320,12)
+#increasing velocity so latch comes up
     drive_base.settings(300,600,100,200)
-    drive_base.settings(999, 999, 999, 999)
-    drive_base.straight(-150)
+#Raising latch and coming back
+    drive_base.straight(-105)
+#To get out of the way
+#drive_base.curve(320,5) - did not work
+#drive_base.curve(320,12) - original but not consistent
+    drive_base.curve(350,12)
+#Updating the speed to go faster (600, 600, 200,200)
+    drive_base.settings(600,600,200,200)
+#Coming back
+    drive_base.straight(-180)
+#Pickup lady
     drive_base.turn(-220)
+#Get to the homezone
     drive_base.straight(300)
 
 #Based on Selection, run the program
